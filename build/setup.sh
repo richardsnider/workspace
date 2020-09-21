2b5f50d27f443068a207fb9852e05b9697f15aac
Richard Snider <richardnsnider@gmail.com>
2020-09-21 17:42:45 +0000

    remove visual studio code install

--- build/setup.sh
+++ build/setup.sh
@@ -27,22 +27,3 @@ cp $BUILD_DIRECTORY/.bashrc $HOME/.bashrc
 # sudo $HOME/aws/install
 # rm -rf $HOME/aws $HOME/awscliv2.zip
 # aws --version
-
-# echo "Install VS Code as directed by https://code.visualstudio.com/docs/setup/linux"
-# curl --silent https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-# install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-# sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-# apt-get update
-# apt-get install -y code
-# rm ~/microsoft.gpg
-# sudo -u user code --install-extension mtxr.sqltools
-# sudo -u user code --install-extension eamodio.gitlens
-# sudo -u user code --install-extension ms-vscode-remote.remote-ssh
-# echo "{
-#     \"workbench.startupEditor\": \"newUntitledFile\",
-#     \"editor.renderWhitespace\": \"all\",
-#     \"editor.minimap.enabled\": false,
-#     \"extensions.ignoreRecommendations\": true,
-#     \"terminal.integrated.shell.linux\": \"/bin/bash\",
-#     \"workbench.statusBar.visible\": false,
-# }" >> $HOME/.config/Code/User/settings.json
